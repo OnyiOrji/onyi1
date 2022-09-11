d924758945f91798a3f6fa4c3006081e9229646a
Onyi Orji <onyiorji1234@gmail.com>
2022-09-11 01:20:52 -0500

    Coded javascript for modal ui

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 // template_k73xlke - temp ID
 // service_hiq03yq - service ID
 // H3Ypoai3lKjQmf5Mj - user ID
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    emailjs
+        .sendForm(
+            'service_hiq03yq',
+            'template_k73xlke',
+            event.target,
+            'H3Ypoai3lKjQmf5Mj'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly on email@gmail.com"
+        )
+    })
+
+    loading.classList += " modal__overlay--visible";
+    setTimeout(() => {
+        console.log('it worked')
+    }, 1000);
+}
